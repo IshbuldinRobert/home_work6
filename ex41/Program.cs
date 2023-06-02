f0ccc831c99951a0b2e880b2e08a70f586c5ef1e
Robert <ishbuldinrobert2002@gmail.com>
2023-06-02 23:11:53 +0500

    hw6 done

---
+++ ex41/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 41:
+// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+Console.Write("Введите количество чисел: ");
+int count = int.Parse(Console.ReadLine());
+int[] arr = new int[count];
+Console.WriteLine("Введите числа: ");
+for (int i = 0; i < count; i++)
+{
+    Console.Write($"{i + 1}: ");
+    arr[i] = int.Parse(Console.ReadLine());
+}
+
+int pos = 0;
+for (int j = 0; j < count; j++)
+{
+    if (arr[j] > 0)
+    {
+        pos++;
+    }
+}
+
+Console.Write($"Количество чисел больше 0: {pos}");
---
+++ ex43/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 43:
+// Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями
+// y = k1 * x + b1
+// y = k2 * x + b2
+// b1, k1, b2 и k2 задаются пользователем
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+Console.WriteLine("Введите коэффициенты уравнений: ");
+double b1 = double.Parse(Console.ReadLine()),
+k1 = double.Parse(Console.ReadLine()),
+b2 = double.Parse(Console.ReadLine()),
+k2 = double.Parse(Console.ReadLine()),
+
+x = (b1 - b2)/(k2 - k1),
+y = k1 * x + b1;
+Console.WriteLine($"Точка пересечения прямых: ({x}; {y})");
